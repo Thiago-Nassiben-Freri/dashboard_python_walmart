9cfd71f48127d0a07ddf70cacefd317cc0feb215
Thiago-Nassiben-Freri <thiagonassibenfreri2@gmail.com>
2026-06-17 19:04:17 -0300

    add: Criado função de carregar os dados na homepage

--- page/homepage.py
+++ page/homepage.py
@@ -5,6 +5,8 @@ import pandas as pd
 # Configurações inicias da página
 st.set_page_config(page_title="Dashboard Walmart Vendas", layout="wide")
 
+# Carrega os dados e deixa eles em memória cache
 @st.cache_data
 def load_data():
-    data = pd.read_csv("")
+    data = pd.read_csv("data/clean/dataset_walmart.csv")
+    return data
