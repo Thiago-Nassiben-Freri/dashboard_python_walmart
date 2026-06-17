db2ac618bb55e88ae9c2ec321687e7d3b4983887
Thiago-Nassiben-Freri <thiagonassibenfreri2@gmail.com>
2026-06-17 18:44:50 -0300

    add: Criado e feito o etl básico do projeto

---
+++ src/etl.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+
+# Carregando os dados
+df = pd.read_csv("data/Walmart_Sales.csv")
+
+# Checando os tipos dos dados
+print(df.dtypes)
+
+# Transformando e padronizando os dados
+df["Date"] = pd.to_datetime(df["Date"], format='%d-%m-%Y') # Converte para o padrão dia, mês e ano
+df["Temperature"] = (df["Temperature"] - 32) * (5/9) # Converte de Fahrenheit para Celsius
+
+# Checando os tipos dos dados (novamente)
+print(df.dtypes)
+
+# Mostra os cinco primeiros resultados
+print(df.head(5))
